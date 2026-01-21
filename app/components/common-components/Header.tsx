3b32a6537db9c08d8f70f620abacc5df277356d7
sumitnegii <negisumit308@gmail.com>
2026-01-21 10:16:23 +0530

    pull

--- app/components/common-components/Header.tsx
+++ app/components/common-components/Header.tsx
@@ -332,7 +332,7 @@ export default function Header() {
                       />
                     </div>
                   ) : (
-                    <div className="w-8 h-8 md:w-9 md:h-9 rounded-full bg-gradient-to-br from-[#EAD7B7] to-[#5A1E12] flex items-center justify-center border-2 border-transparent group-hover:border-[#5A1E12] transition-colors">
+                    <div className="w-8 h-8 md:w-9 md:h-9 rounded-full bg-linear-to-br from-[#EAD7B7] to-[#5A1E12] flex items-center justify-center border-2 border-transparent group-hover:border-[#5A1E12] transition-colors">
                       <User className="w-4 h-4 text-white" />
                     </div>
                   )}
@@ -422,7 +422,7 @@ export default function Header() {
           <div className="flex items-center gap-3">
             {user ? (
               <>
-                <div className="w-10 h-10 rounded-full bg-gradient-to-br from-[#EAD7B7] to-[#5A1E12] flex items-center justify-center">
+                <div className="w-10 h-10 rounded-full bg-linear-to-br from-[#EAD7B7] to-[#5A1E12] flex items-center justify-center">
                   {user.profileImage ? (
                     <Image
                       src={user.profileImage}
