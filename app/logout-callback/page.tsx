e471f34a4f4240c58768cdd620f64f4c1abbe5b2
Shubham-1007 <techies@crobstacle.com>
2026-02-26 13:13:37 +0530

    Updated Logout callback File

--- app/logout-callback/page.tsx
+++ app/logout-callback/page.tsx
@@ -72,25 +72,7 @@ function LogoutCallbackContent() {
         />
 
         {/* Spinner */}
-        <svg
-          className="w-10 h-10 animate-spin text-[#5A1E12]"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <circle
-            className="opacity-20"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            strokeWidth="3"
-          />
-          <path
-            className="opacity-90"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-          />
-        </svg>
+        <div className="w-8 h-8 rounded-full border-2 border-[#5A1E12]/20 border-t-[#5A1E12] animate-spin" />
 
         {/* Text */}
         <div className="text-center">
